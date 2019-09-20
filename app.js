2f603355ac5fcc11deef0adc0e118e74bd9c7a8e
kosem37 <53511026+kosem37@users.noreply.github.com>
2019-09-20 08:51:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var config = {
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
   recaptcha_sitekey: '6LdI2CMTAAAAAGMWJ-lJ3k2PXrjlxgcjuzmeq5kq',  // <----- EDIT ME!
-  redirect_uri: 'http://neodice.ml/',
+  redirect_uri: 'https://super999.heroku.com',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
   chat_uri: 'https://a-chat-server.herokuapp.com',
